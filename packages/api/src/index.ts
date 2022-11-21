60aa6aa802c6c15a238f5be0abdb3de2c5ebc971
Oren Ghelerter <orenghe@gmail.com>
2022-11-21 17:02:01 +0200

    set up authentication

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,26 @@
 import { Hono } from 'hono';
-import { poweredBy } from 'hono/powered-by';
+import { jwt } from 'hono/jwt';
 
-const app = new Hono();
+interface Env {
+  SUPABASE_URL: string;
+  SUPABASE_ANON_KEY: string;
+  SUPABASE_JWT_SECRET: string;
+}
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.use('/*', (c, next) => {
+  const auth = jwt({
+    secret: c.env.SUPABASE_JWT_SECRET,
+  });
+  return auth(c, next);
+});
 
 app.get('/', (c) => {
   return c.text('Hello world');
+});
+app.get('/m', (c) => {
+  return c.text('mmmmm');
 });
 
 export default app;
